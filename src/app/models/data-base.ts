4d15eaf95109158dec7ea18450e6e33186504cff
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-10-11 14:02:40 +0200

    initial commit'

---
+++ src/app/models/data-base.ts
@@ -0,0 +1,32 @@
+export const QUOTES = [
+  {
+    author: 'Jonathan Carroll',
+    quotation: 'To, że milczę, nie znaczy, że nie mam nic do powiedzenia.',
+    votes: -4,
+  },
+  {
+    author: 'Suzanne Collins',
+    quotation: 'Masz w sobie tyle uroku co zdechła dżdżownica.',
+    votes: 2,
+  },
+  {
+    author: 'George Eliot',
+    quotation: 'Nigdy nie jest za późno, by stać się tym, kim chcemy być.',
+    votes: 1,
+  },
+  {
+    author: 'Stanisław Lem',
+    quotation: 'Bądź dobrej myśli, bo po co być złej.',
+    votes: null,
+  },
+  {
+    author: 'Tove Jansson',
+    quotation: 'Mówienie tak bardzo przeszkadza w myśleniu.',
+    votes: -8,
+  },
+  {
+    author: 'Peter Lippert',
+    quotation: 'Życia nie można wybrać, ale można z niego coś zrobić.',
+    votes: null,
+  }
+];
