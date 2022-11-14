c3658604ef41ed729e9ae2c14d1c3cf11171c12f
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-11-14 18:38:13 +0100

    finished app

--- src/app/vote-buttons/vote-buttons.component.ts
+++ src/app/vote-buttons/vote-buttons.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { IQuotation } from '../models/quotation';
 
 @Component({
   selector: 'app-vote-buttons',
@@ -7,9 +6,5 @@ import { IQuotation } from '../models/quotation';
   styleUrls: ['./vote-buttons.component.css'],
 })
 export class VoteButtonsComponent {
-  constructor() {}
-
   @Output() voteValue = new EventEmitter<boolean>();
-  @Input() classPlus: string;
-  @Input() classMinus: string;
 }
